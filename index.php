9fc4886e9030c9edbfbce61c1fcce43e7092cc60
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 12:08:55 +0200

    ex4 partie4 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$name = 'Castellano';
+$firstname = 'Guylaine';
+$age = 46
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex4 partie4 php</title>
+</head>
+<body>
+<p>Bonjour <?php echo $name . ' ' . $firstname ?> tu as <?php echo $age ?> ans</p>
+</body>
+</html>
